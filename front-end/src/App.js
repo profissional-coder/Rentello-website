fb43314d13b709260e4a5304835265ffb293bf93
your_github_ <your_email@example.com>
2020-11-11 23:50:32 +0200

    post

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,31 +8,34 @@ const App = () => {
   
   return (
     <div>
-      <div className='PostTitle'>
-        <label>Post Title  </label>
-        <input type='text' placeholder="Write Post Title Here"></input>
-    </div>
-
-    <div className='A'>
-    <div className='Description'>
-        <p>Description</p>
-        <textarea></textarea>
-    </div>
-       <div className='DateRange'>
-         <p>Date Range</p>
-         <input type="date"></input>
-         <p>Category</p>
-          <select>
-            <p>Category</p>
-            <option>a</option>
-            <option>a</option>
+      <div className='ChooseaDepartment'> 
+        <span> 1 </span>
+        <strong>Choose a Department</strong>
+        </div>
+        <div className="section">
+        <label> Choose the main section </label>
+        <select>
+            <option>Cars</option>
+            <option>Mobile Tablet</option>
             <option>a</option>
           </select>
-       </div>
-      </div>
-        <div className='B'>
-       <div className="Locations">
-         <label for="Locations">Location </label>
+        </div>
+           
+      <div className='Details'> 
+        <span> 2 </span>
+        <strong>Details</strong>
+        </div>
+        <div className="A">
+        <div className="Title">
+        <span>Post Title  </span>
+        <input type='text' placeholder="Write Post Title Here"></input>
+        <span>Description </span>
+        <textarea></textarea>
+        </div>
+        
+        
+        <div className='Location'>
+        <label for="Locations">Location </label>
             <select>
               <option>Amman</option>
               <option>Zarqa</option>
@@ -40,31 +43,47 @@ const App = () => {
               <option>Karak</option>
               <option>Tafila</option>
             </select>
-        </div>
-        <div className='PhoneNumber'>
-          <label>Phone Number </label>
-          <input type='Number' placeholder ='079'></input>
-        </div>
-        </div>
-        <div className="C">
-       <div className='Price'>
-         <label>Price </label>
-         <input type="Number" placeholder="10"></input> / 
+
+          <label>Price </label>
+          <input type="Number" placeholder="10"></input> 
           <input type="text" placeholder="hr"></input>
+
+          <div className='IMG'>
+        
+         <input  type="file" name="image"></input>
        </div>
+       
+        </div>
+      </div>
+      <div className='DateRange'> 
+        <span> 3 </span>
+        <strong>Date Range</strong>
+        </div>
+        <div className='Date'>
+         <label>Start Date </label>
+         <input type="datetime-local"></input>
+         <label>End Date </label>
+         <input type="datetime-local"></input>
        </div>
 
-       <div className="D">
-       <div className='IMG'>
-         <label>IMG </label>
-         <input type="file" placeholder="up load" id="img" name="img" accept="image/png, image/jpeg"></input>
-         
-       </div>
-       </div>
+
+      <div className='contactInformation'> 
+        <span> 4 </span>
+        <strong>Contact Information</strong>
+        </div>
+        <div className='PhoneNumber'>
+          <label>Name </label>
+          <input type='text' placeholder ='Name'></input>
+          <label>Phone Number </label>
+          <input type='tel' placeholder ='079'></input>
+        </div>
+        
+            <div className='Save'>
+              <button> Save </button>
+            </div>
     </div>
     
   );
 };
 export default App;
-
 
