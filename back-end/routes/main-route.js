39d2e951fdcf46d96016879ce9b5ddd33bac4e17
malek88888 <jmzomalek@gmail.com>
2020-11-11 21:18:00 +0200

    backend of the post schema crud is done but still need to connect tow fils to gither for better feature

--- back-end/routes/main-route.js
+++ back-end/routes/main-route.js
@@ -1,8 +1,63 @@
 const express = require('express');
 const mainRouter = express.Router();
+const {connection,showPosts} = require("../schemas/postSchema")
 
 mainRouter.get('/', (req, res) => {
   res.json('HELLO WORLD');
 });
+// mainRouter.get('/posts', showPosts
+// );
+mainRouter.post('/posts/create',async (req, res) => {
+ await connection.query(`insert into posts (user_id,comment_id,category_id,price,post_date,category,location,from_date,to_date)
+  values (${req.body.user_id},${req.body.comment_id},${req.body.category_id},${req.body.price},
+      '${req.body.post_date}',"${req.body.category}","${req.body.location}",
+      "${req.body.from_date}",'${req.body.to_date}')`
+  ,(err,result)=>{
+  console.log(result);
+  res.json(result)
 
+      if (err) {
+          console.log(err);
+        res.json(err)}
+  })
+});
+mainRouter.get('/posts', (req, res) => {
+  connection.query(`select * from posts`,(err,result)=>{
+    if (err) throw err
+    console.log(result);
+    res.json(result)
+        })
+});
+mainRouter.delete('/posts/delete-all', (req, res) => {
+  connection.query(`delete from posts WHARE `,(err,result)=>{
+    if (err) throw err
+    console.log(result);
+        })
+});
+mainRouter.delete('/posts/delete', (req, res) => {
+  connection.query(`DELETE FROM posts WHERE post_id='${req.body.post_id}'`,(err,result)=>{
+    if (err) throw err
+    console.log(result);
+    res.json(result)
+        })
+});
+mainRouter.put('/posts/add', (req, res) => {
+  
+  connection.query(`UPDATE posts 
+  SET 
+  category_id=${req.body.category_id},
+  price=${req.body.price},
+  post_date='${req.body.post_date}',
+  category="${req.body.category}",location="${req.body.location}",
+  from_date= "${req.body.from_date}",to_date='${req.body.to_date}' 
+  WHERE  
+  post_id='${req.body.post_id}' `
+  ,(err,result)=>{
+  console.log(result);
+  res.json(result) 
+      if (err) {
+          console.log(err);}  
+  })
+  }  
+)
 module.exports = mainRouter;
---
+++ back-end/schemas/postSchema.js
@@ -0,0 +1,98 @@
+const mysql =require("mysql2")
+const { createConnection } = require("mysql2/promise")
+require("dotenv").config()
+const express = require('express'); 
+const app = express();
+const connection= mysql.createConnection({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.DB_NAME
+})
+connection.connect((err) => {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+const addPost=(post)=>{
+connection.query(`insert into posts (user_id,comment_id,category_id,price,post_date,category,location,from_date,to_date)
+values (${post.user_id},${post.comment_id},${post.category_id},${post.price},
+    '${post.post_date}',"${post.category}","${post.location}",
+    "${post.from_date}",'${post.to_date}')`
+,(err,res)=>{
+console.log(res);
+    if (err) {
+        console.log(err);}
+    
+})
+}  
+const test1={
+    user_id:0
+    ,comment_id:0
+    ,category_id:0,
+    price:12,
+    category:"pants",
+    post_date:'2018-10-20',
+    location:"zarqa",
+    from_date:'2018-10-20',
+    to_date:'2018-10-20'
+}
+const showPosts=(req,res)=>{
+    connection.query(`select * from posts`,(err,result)=>{
+if (err) throw err
+console.log(result);
+// res.json(result)
+    })
+}
+showPosts()
+// addPost(test1)  
+const deleteAllPosts=()=>{
+    connection.query(`delete from posts WHARE `,(err,res)=>{
+if (err) throw err
+console.log(res);
+    })
+}
+// deleteAllPosts()
+const deletePost=(id)=>{
+    connection.query(`DELETE FROM posts WHERE post_id='${id}'`,(err,res)=>{
+if (err) throw err
+console.log(res);
+    })
+
+} 
+// deletePost(14)
+
+const updatePost=(post)=>{
+    connection.query(`UPDATE posts 
+    SET 
+    category_id=${post.category_id},
+    price=${post.price},
+    post_date='${post.post_date}',
+    category="${post.category}",location="${post.location}",
+    from_date= "${post.from_date}",to_date='${post.to_date}' 
+    WHERE  
+    post_id='${post.post_id}' `
+    ,(err,res)=>{
+    console.log(res);
+    
+        if (err) {
+            console.log(err);}
+        
+    })
+    }  
+    const test2={
+        post_id:15,
+        user_id:0
+        ,comment_id:0
+        ,category_id:0,
+        price:12,
+        category:"pants2",
+        post_date:'2017-10-20',
+        location:"Amman",
+        from_date:'2017-10-20',
+        to_date:'2017-10-20'
+    }
+    // updatePost(test2)
+module.exports={connection,showPosts}
