e02f457045dcf81b0ae5ac2b69ce388499d6c99d
Rama-Qandeel <rama.qndeel@gmail.com>
2020-11-11 19:40:28 +0200

    check the password

--- back-end/controllers/main-controller.js
+++ back-end/controllers/main-controller.js
@@ -1,7 +1,7 @@
 const connection = require("../db");
 const express = require("express");
-const bcrypt=require ("bcrypt")
-require('dotenv').config();
+const bcrypt = require("bcrypt");
+require("dotenv").config();
 const getAllUsers = (req, res) => {
   const command = `SELECT * FROM users`;
   connection.query(command, (err, result) => {
@@ -10,10 +10,10 @@ const getAllUsers = (req, res) => {
     res.json(result);
   });
 };
-const addUser = async(req, res) => {
+const register = async (req, res) => {
   const query = `INSERT INTO users (Fullname, email,password,city,address,RegDate,dob) VALUES (?, ?, ?, ?,?,now(),?)`;
-let { Fullname, email, password, city, address, dob } = req.body;
-  password= await bcrypt.hashSync(password,Number(process.env.SALT));
+  let { Fullname, email, password, city, address, dob } = req.body;
+  password = await bcrypt.hashSync(password, Number(process.env.SALT));
   const data = [Fullname, email, password, city, address, dob];
   connection.query(query, data, (err, result) => {
     if (err) {
@@ -24,10 +24,26 @@ let { Fullname, email, password, city, address, dob } = req.body;
   });
 };
 
-
-
+const login = (req, res) => {
+  const query = `SELECT email ,password FROM users WHERE email=?`;
+  const { email, password } = req.body;
+  const data = [email, password];
+  let r = [];
+  connection.query(query, data, async (err, result) => {
+    if (err) throw err;
+    console.log("result :", result[0].password);
+    if (result.length) {
+      if (await bcrypt.compare(password, result[0].password)) {
+        res.json("correct");
+      }else{
+        res.json("not correct"); 
+      }
+    }
+  });
+};
 
 module.exports = {
   getAllUsers,
-  addUser,
+  register,
+  login,
 };
--- back-end/routes/main-route.js
+++ back-end/routes/main-route.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mainRouter = express.Router();
-const { getAllUsers, addUser } = require("../controllers/main-controller");
+const { getAllUsers, register,login } = require("../controllers/main-controller");
 
 mainRouter.get("/", (req, res) => {
   res.json("HELLO WORLD");
 });
 
 mainRouter.get("/user", getAllUsers);
-mainRouter.post("/register", addUser);
+mainRouter.post("/register", register);
+mainRouter.post("/login",login);
 
 module.exports = mainRouter;
