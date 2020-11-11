63e453019515c42e7deeb43042eb412ec2643a02
your_github_ <your_email@example.com>
2020-11-11 18:41:41 +0200

    post

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,29 +1,70 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-// Class component
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div>
-        <h1>HELLO WORLD</h1>
-      </div>
-    );
-  }
-}
 
-/* 
-// functional component
 const App = () => {
+  
+  
+  
   return (
     <div>
-      <h1>HELLO WORLD</h1>
+      <div className='PostTitle'>
+        <label>Post Title  </label>
+        <input type='text' placeholder="Write Post Title Here"></input>
     </div>
+
+    <div className='A'>
+    <div className='Description'>
+        <p>Description</p>
+        <textarea></textarea>
+    </div>
+       <div className='DateRange'>
+         <p>Date Range</p>
+         <input type="date"></input>
+         <p>Category</p>
+          <select>
+            <p>Category</p>
+            <option>a</option>
+            <option>a</option>
+            <option>a</option>
+          </select>
+       </div>
+      </div>
+        <div className='B'>
+       <div className="Locations">
+         <label for="Locations">Location </label>
+            <select>
+              <option>Amman</option>
+              <option>Zarqa</option>
+              <option>Irbid</option>
+              <option>Karak</option>
+              <option>Tafila</option>
+            </select>
+        </div>
+        <div className='PhoneNumber'>
+          <label>Phone Number </label>
+          <input type='Number' placeholder ='079'></input>
+        </div>
+        </div>
+        <div className="C">
+       <div className='Price'>
+         <label>Price </label>
+         <input type="Number" placeholder="10"></input> / 
+          <input type="text" placeholder="hr"></input>
+       </div>
+       </div>
+
+       <div className="D">
+       <div className='IMG'>
+         <label>IMG </label>
+         <input type="file" placeholder="up load" id="img" name="img" accept="image/png, image/jpeg"></input>
+         
+       </div>
+       </div>
+    </div>
+    
   );
 };
 export default App;
-*/
+
+
