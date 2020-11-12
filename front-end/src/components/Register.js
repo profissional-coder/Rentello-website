97456f5908db41db16faadbbc5c79814e59032bd
your_github_username <rabi.abdelrahman@gmail.com>
2020-11-12 07:47:35 +0200

    updating registration form

--- front-end/src/components/Register.js
+++ front-end/src/components/Register.js
@@ -1,50 +1,114 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Register extends Component {
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit} className="submit-form">
-          <div>
-            <label>User Name </label>
-            <input type="text" placeholder="enter your username"></input>
-          </div>
-          <br></br>
-          <div>
-            <label>Email </label>
-            <input type="email" placeholder="enter your email"></input>
-          </div>
-          <br></br>
-          <div>
-            <label>Password </label>
-            <input type="password" placeholder="enter your password"></input>
-          </div>
-          <br></br>
-          <div>
-            <label>Confirm Password </label>
-            <input type="password" placeholder="confirm your password"></input>
-          </div>
-          <br></br>
-          <div>
-            <label>City </label>
-            <input type="text" placeholder="your City"></input>
-          </div>
-          <br></br>
-          <div>
-            <label>Address </label>
-            <input type="text" placeholder="your address"></input>
-          </div>
-          <br></br>
-          <div>
-            <label>Date Of Birth </label>
-            <input type="date"></input>
-          </div>
-          <br></br>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    );
-  }
-}
+const Register = () => {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    password2: "",
+    city: "",
+    address: "",
+    DOB: "",
+  });
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div className="Register-container">
+      <form>
+        <h1>Register User</h1>
+        <div>
+          <label>Username </label>
+          <br />
+          <input
+            type="text"
+            name="username"
+            placeholder="enter your username"
+            value={values.username}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <br />
+        <div>
+          <label>Email </label>
+          <br />
+          <input
+            type="email"
+            name="email"
+            placeholder="enter your email"
+            value={values.email}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <br />
+        <div>
+          <label>Password </label>
+          <br />
+          <input
+            type="password"
+            name="password"
+            placeholder="enter password"
+            value={values.password}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <br />
+        <div>
+          <label>Confirm Password </label>
+          <br />
+          <input
+            type="password"
+            name="password2"
+            placeholder="confirm password"
+            value={values.password2}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <br />
+        <div>
+          <label>City </label>
+          <br />
+          <input
+            type="text"
+            name="city"
+            placeholder="your City"
+            value={values.city}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <br />
+        <div>
+          <label>Address </label>
+          <br />
+          <input
+            type="text"
+            name="address"
+            placeholder="your address"
+            value={values.address}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <br />
+        <div>
+          <label>Date Of Birth </label>
+          <br />
+          <input
+            type="date"
+            name="DOB"
+            value={values.password}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <br />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
 
 export default Register;
