c145a361473c795bfba9aad25e9d45ff52321a6c
malek88888 <jmzomalek@gmail.com>
2020-11-12 19:10:54 +0200

    new details edited and the post schema crud is okey

--- back-end/routes/main-route.js
+++ back-end/routes/main-route.js
@@ -5,19 +5,19 @@ const {connection,showPosts} = require("../schemas/postSchema")
 mainRouter.get('/', (req, res) => {
   res.json('HELLO WORLD');
 });
-mainRouter.get('/posts/image', (req, res) => {
-  connection.query(`select * from image`,(err,result)=>{
-    if (err) throw err
-    console.log(result);
-    res.json(result)
-        })});
+// mainRouter.get('/posts/image', (req, res) => {
+//   connection.query(`select * from image`,(err,result)=>{
+//     if (err) throw err
+//     console.log(result);
+//     res.json(result)
+//         })});
 
 mainRouter.post('/posts/create',async (req, res) => {
   connection.query(`insert into posts (user_id,comment_id,category_id,price,
   post_date,category,location,from_date,to_date,image_url)
   values (${req.body.user_id},${req.body.comment_id},${req.body.category_id},${req.body.price},
       '${req.body.post_date}',"${req.body.category}","${req.body.location}",
-      "${req.body.from_date}",'${req.body.to_date}',${req.body.image_url})`
+      "${req.body.from_date}",'${req.body.to_date}','${req.body.image_url}')`
   ,(err,result)=>{
   console.log(result);
   res.json(result)
@@ -45,17 +45,21 @@ mainRouter.delete('/posts/delete-all', (req, res) => {
   connection.query(`delete from posts WHARE `,(err,result)=>{
     if (err) throw err
     console.log(result);
+    res.json(result)
+
         })
 });
-mainRouter.delete('/posts/delete/:post_id', (req, res) => {
-  connection.query(`DELETE FROM posts WHERE post_id='${req.params}'`,(err,result)=>{
+mainRouter.delete('/posts/:post_id', (req, res) => {
+  console.log("params",req.params);
+
+  connection.query(`DELETE FROM posts WHERE post_id='${req.params.post_id}'`,(err,result)=>{
     if (err) throw err
     console.log("params",req.params);
-    // console.log(result);
+    console.log(result);
     res.json(result)
         })
 });
-mainRouter.put('/posts/add', (req, res) => {
+mainRouter.put('/posts/update', (req, res) => {
   
   connection.query(`UPDATE posts 
   SET 
@@ -63,7 +67,8 @@ mainRouter.put('/posts/add', (req, res) => {
   price=${req.body.price},
   post_date='${req.body.post_date}',
   category="${req.body.category}",location="${req.body.location}",
-  from_date= "${req.body.from_date}",to_date='${req.body.to_date}' 
+  from_date= "${req.body.from_date}",to_date='${req.body.to_date}',
+  image_url="${req.body.image_url}"
   WHERE  
   post_id='${req.body.post_id}' `
   ,(err,result)=>{
