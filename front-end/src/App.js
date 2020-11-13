6e4f273ab6ffa54f31164e19ff3f346b2ea74a45
your_github_ <your_email@example.com>
2020-11-13 02:26:22 +0200

    post

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -38,91 +38,36 @@ const App = () => {
     
   };
   return (
-    <div>
-      <div className='A'>
-      <div className='ChooseaDepartment'> 
-        <span> 1 </span>
-        <strong>Choose a Department</strong>
-        </div>
-        <div className="section">
-        <label> Choose the main section </label>
-        <select onChange={(e) => {
-          setCategory(e.target.value)
-        }}>
-            <option>Cars</option>
-            <option>Mobile Tablet</option>
-          </select>
-        </div>
-           </div>
+    <div class="padding-all">
+		<div class="design">
+			<div class="mail-form-agile">
+				
+             <input onChange={(e) =>{setTitle(e.target.value)}} type='text' placeholder="Write Post Title Here"></input>
 
-
-
-           
-             <div className='Details'> 
-                    <span> 2 </span>
-                    <strong>Details</strong>
-            </div>
-             <div className='B'>
-             <div className="Title">
-              <span>Post Title  </span>
-              <input onChange={(e) =>{setTitle(e.target.value)}} type='text' placeholder="Write Post Title Here"></input>
-              <span>Description </span>
-              <textarea onChange={(e) => {setDescription(e.target.value)}}></textarea>
-             </div>
-        
-        
-             <div className='Location'>
-              <label for="Locations">Location </label>
-              <select onChange={(e) => {setLocation(e.target.value)}}>
-                <option value='1'>Amman</option>
-                <option>Zarqa</option>
-                <option>Irbid</option>
-                <option>Karak</option>
-                <option>Tafila</option>
-             </select>
-          <div className='Price'>
-              <label>Price </label>
-              <input onChange={(e) => {setprice(e.target.value)}} type="Number" placeholder="10"></input> 
-              <input  type="text" placeholder="hr"></input>
-          </div>
-          <div className='IMG'>
-            <input onChange={(e) => {setIMG(e.target.value)}}  type="file" name="image"></input>
-          </div>
-        </div>
+             <input onChange={(e) => {setprice(e.target.value)}} type="Number" placeholder="Price"></input>
+             <select onChange={(e) => {setCategory(e.target.value)}} placeholder="Select Category">
+                            <option>Cars</option>
+                            <option>Mobile Tablet</option>
+                    </select>
+             <select onChange={(e) => {setLocation(e.target.value)}}>
+                        <option value='0'>Select Country:</option>
+                        <option value='1'>Amman</option>
+                        <option >Zarqa</option>
+                        <option>Irbid</option>
+                        <option>Karak</option>
+                        <option>Tafila</option>
+                     </select>
+                   
+                     <span>Start Date</span><input onChange={(e) => {setStartDate(e.target.value)}} type="datetime-local"></input>
+                     <span>End Date</span><input onChange={(e) => {setEndDate(e.target.value)}} type="datetime-local"></input>
+             <input onChange={(e) => {setName(e.target.value)}} type='text' placeholder ='Name'></input>
+             <input onChange={(e) => {setPhoneNumber(e.target.value)}} type='tel' placeholder ='Phone Number'></input>
+             <textarea placeholder='Description' onChange={(e) => {setDescription(e.target.value)}}></textarea>
+             <input onChange={(e) => {setIMG(e.target.value)}}  type="file" name="image"></input>
+				     <input onClick={SavePost} type="submit" value='Save'></input>
       </div>
-
-
-
-      <div className='DateRange'> 
-        <span> 3 </span>
-        <strong>Date Range</strong>
-        </div>
-        <div className='C'>
-        <div className='Date'>
-         <label>Start Date </label>
-         <input onChange={(e) => {setStartDate(e.target.value)}} type="datetime-local"></input>
-         <label>End Date </label>
-         <input onChange={(e) => {setEndDate(e.target.value)}} type="datetime-local"></input>
-       </div>
-         </div>
-
-      <div className='contactInformation'> 
-        <span> 4 </span>
-        <strong>Contact Information</strong>
-        </div>
-        <div className='D'>
-        <div className='PhoneNumber'>
-          <label>Name </label>
-          <input onChange={(e) => {setName(e.target.value)}} type='text' placeholder ='Name'></input>
-          <label>Phone Number </label>
-          <input onChange={(e) => {setPhoneNumber(e.target.value)}} type='tel' placeholder ='079'></input>
-        </div>
-        </div>
-        <div className='E'>
-            <div className='Save'>
-              <button onClick={SavePost}> Save </button>
-            </div>
-            </div>
+		  <div class="clear"> </div>
+		</div>
     </div>
     
   );
