bebf13ce0479c68e3474f2bf0be70e10a18aab3f
Rama-Qandeel <rama.qndeel@gmail.com>
2020-11-15 13:52:22 +0200

    create new branch

--- back-end/db.js
+++ back-end/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
-
+require('dotenv').config();
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
+console.log('PASS :',process.env.DB_PASS);
 
 connection.connect((err) => {
   if (err) {
--- back-end/routes/main-route.js
+++ back-end/routes/main-route.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const mainRouter = express.Router();
+const { getAllUsers, register,login,deleteAccount } = require("../controllers/main-controller");
 
-mainRouter.get('/', (req, res) => {
-  res.json('HELLO WORLD');
-});
-
+mainRouter.get("/user", getAllUsers);
+mainRouter.post("/register", register);
+mainRouter.post("/login",login);
+mainRouter.delete("/delete/:user_id",deleteAccount);
 module.exports = mainRouter;
