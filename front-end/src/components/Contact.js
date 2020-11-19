48e33608aac8fb0eceff2f49f90f37a938a4cd06
your_github_ <your_email@example.com>
2020-11-19 14:18:58 +0200

    postpage

--- front-end/src/components/Contact.js
+++ front-end/src/components/Contact.js
@@ -1,38 +1,45 @@
 import React, { useState } from "react";
 import axios from 'axios';
 const Contact = () => {
-  const [contact, setContact] = useState([]);
-  const [name, setName] = useState("");
-  const [subject, setSubject] = useState("");
-  const [email, setEmail] = useState("");
-  const [Message, setMessage] = useState("");
-
   
-  const CreateAllContact = (infoArgumnt) => {
+  const [contact, setContact] = useState({
+    name:"",
+    subject:"",
+    email:"",
+    Message:""
+  });
+  
+  const handleChange = (e) => {
+    setContact({
+      ...contact,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
     axios
-      .post('http://localhost:5000/contact', infoArgumnt)
-      .then((response) => {
-        console.log("response",response)
-        const newArray = [...contact];
-        newArray.push(response.data)
-        setContact(newArray);
+      
+      .post("http://localhost:5000/contact", contact)
+      .then((result) => {
+          console.log("result : ",result);
+
       })
       .catch((err) => {
-        console.log('RESULT: ', err);
+        console.log("ERR : ", err);
       });
   };
-  const Send= () => { 
-    CreateAllContact({name:name,subject:subject,email:email,Message:Message})
-  };
+
   return (
     <div className='Contact'>
       <div className='con'>
-      <input onChange={(e) =>{setName(e.target.value)}} type='text' placeholder='Name'></input>
-      <input onChange={(e) =>{setEmail(e.target.value)}} type='email' placeholder='email'></input>
-      <input onChange={(e) =>{setSubject(e.target.value)}} type='text' placeholder='Subject'></input>
-      <input onChange={(e) =>{setMessage(e.target.value)}} type='text' placeholder='Message'></input><br></br>
+      <form onSubmit={handleSubmit}>
+      <input  type='text' name='name' placeholder='Name' value={contact.name} onChange={handleChange}></input>
+      <input  type='email' name='email' placeholder='email' value={contact.email} onChange={handleChange}></input>
+      <input  type='text' name='subject' placeholder='Subject' value={contact.subject} onChange={handleChange}></input>
+      <input  type='text' name='Message' placeholder='Message' value={contact.Message} onChange={handleChange}></input><br></br>
       
-      <button onClick={Send}> Send </button><br></br>
+      <button type='submit'> Send </button><br></br>
+      </form>
       </div>
     </div>
   );
